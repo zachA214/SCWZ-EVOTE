66920b84ad745439d31d296671feea2a5439895e
Zachary Adelson <zachary.adelson@wsu.edu>
2024-04-23 15:30:42 -0700

    ballot and vote

---
+++ WpfApp1/WpfApp1/Ballot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Ballot
+    {
+        private string voterName;
+        private bool voteMade;
+
+        public Ballot(string userName)
+        {
+            voterName = userName;
+            voteMade = false;
+        }
+    }
+}
