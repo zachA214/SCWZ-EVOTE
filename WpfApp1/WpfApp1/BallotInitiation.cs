21cc8a9451891c80fd470d68ccad61c720aad018
zachary <zgadelson@comcast.net>
2024-04-24 14:37:51 -0700

    things and something

---
+++ WpfApp1/WpfApp1/BallotInitiation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class BallotInitiation
+    {
+        public BallotInitiation() { }
+
+        public Ballot createBallot()
+        {
+            var results = new Ballot();
+            return results;
+        }
+
+        public Ballot createBallot(string voterName)
+        {
+            var results = new Ballot(voterName);
+            return results;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/NewUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class NewUser
+    {
+        private string userName;
+        public NewUser(string name)
+        {
+            userName = name;
+        }
+
+        public void register()
+        {
+
+        }
+    }
+}
